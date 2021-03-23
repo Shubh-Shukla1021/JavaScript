bd73c03b5cb5f8f0066cf6ebc21281bdf8584fba
Shubham <shubh.shukla1021@gmail.com>
2021-03-23 21:19:48 +0530

    added javascript

---
+++ index.js
@@ -0,0 +1,115 @@
+
+
+// Program to print sum of all number in array
+
+//  let numeric_array = [1,2,3,4,5];
+//  let sum = 0;
+//  for(i=0;i<numeric_array.length;i++){
+//      sum += numeric_array[i];
+//      }
+// console.log(sum);
+
+
+
+
+
+
+
+
+//Program to find remainder and quotient
+
+let a = parseInt(prompt("Enter first value"));
+let b = parseInt(prompt("Enter second value"));
+
+let remainder = a%b;
+let quotient = a/b;
+ if(remainder!=0){
+     quotient = a/b;
+     console.log("Quotient is" ,parseInt(quotient));
+     console.log("Remainder is",remainder)
+ }
+ else{
+     console.log("Remainder is",remainder);
+     console.log("quotient is",quotient);
+ }
+
+
+
+
+
+
+
+
+
+
+//Program for addition subtraction multiplication and divison
+
+// var output = 0;
+// var a =  parseInt(prompt("Enter the first number"));
+// var b = parseInt(prompt("Enter the second number"));
+
+// var operation = prompt("Enter name of opeartion you want to perform like addition ,subtraction, multiplication, divison and modulus");
+
+// function addition(){
+//     output = a + b;
+// }
+
+// function substraction(){
+//     output = a - b;
+// }
+
+// function multiplication(){
+//     output = a * b;
+// }
+
+// function division(){
+//     output = a/b;
+// }
+
+// function modulus(){
+//     output = a%b;
+// }
+
+// switch(operation){
+//     case "addition" :
+//         addition();
+//         console.log(output);
+//         break;
+    
+//     case "subtraction" :
+//         subtraction();
+//         console.log(output);
+//         break;
+    
+//     case "multiplication" :
+//         multiplication();
+//         console.log(output);
+//         break;
+
+//     case "division" :
+//         division();
+//         console.log(output);
+//         break; 
+        
+//     case "modulus" :
+//         modulus();
+//         console.log(output);
+//         break;    
+
+// }
+
+
+
+
+//Program to access items of object
+
+// var person = { firstname : "John",  lastname : "Doe",  age : 21,  eyecolor : "brown" } 
+
+// console.log(person.firstname);
+// console.log(person.lastname);
+// console.log(person.age);
+// console.log(person.eyecolor);
+
+
+
+
